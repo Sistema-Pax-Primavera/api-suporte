9ab6dfd70c37331d267226eaf449cead59f21737
GiovaneMiranda15 <mirandagiovane7@gmail.com>
2023-09-11 17:47:23 -0400

    Criação das migrações para CRUD de usuários

---
+++ database/migrations/1694467468356_usuarios.ts
@@ -0,0 +1,77 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+/**
+ * Classe de migração para criar a tabela 'usuarios'.
+ *
+ * Esta migração verifica se a tabela 'usuarios' já existe no banco de dados.
+ * Se não existir, a tabela é criada com as colunas especificadas.
+ * Se já existir, nada é feito no método 'up'.
+ *
+ * @class
+ * @extends BaseSchema
+ */
+export default class extends BaseSchema {
+  /**
+   * Nome do esquema no banco de dados.
+   *
+   * @protected
+   * @type {string}
+   */
+  protected schemaName: string = 'public'
+
+  /**
+   * Nome da tabela que esta migração cria.
+   *
+   * @protected
+   * @type {string}
+   */
+  protected tableName: string = 'usuarios'
+
+  /**
+   * Método 'up' da migração.
+   * Cria a tabela 'usuarios' se ela não existir.
+   *
+   * @public
+   * @returns {Promise<void>}
+   */
+  public async up(): Promise<void> {
+    const hasTable = await this.schema
+      .withSchema(this.schemaName)
+      .hasTable(this.tableName)
+
+    if (!hasTable) {
+      this.schema.withSchema(this.schemaName)
+        .createTable(this.tableName, (table) => {
+          table.increments('id').primary()
+          table.integer('unidade_id').notNullable().unsigned().references('id').inTable('public.unidades').onDelete('NO ACTION').onUpdate('NO ACTION')
+          table.integer('setor_id').notNullable().unsigned().references('id').inTable('public.setores').onDelete('NO ACTION').onUpdate('NO ACTION')
+          table.integer('funcao_id').notNullable().unsigned().references('id').inTable('public.funcoes').onDelete('NO ACTION').onUpdate('NO ACTION')
+          table.string('nome', 150).notNullable()
+          table.string('cpf', 150).notNullable()
+          table.string('senha', 50).notNullable()
+          table.decimal('porcentagem_desconto', 10, 2).nullable().defaultTo(0).comment('Porcentagem máxima de desconto que o usuário pode aplicar nos recebimentos. Caso seja definido como 0, segue o padrão das regras de desconto.')
+          table.timestamp('ultimo_acesso').nullable().comment('Data/Hora do ultimo acesso do usuário.')
+          table.timestamp('ultimo_sincronismo').nullable().comment('Caso seja um cobrador/vendedor. Armazena Data/Hora do ultimo sincronismo realizado.')
+          table.time('hora_liberacao').nullable().comment('Caso seja um cobrador, ele terá acesso ao aplicativo apenas após esse horário. Se definido como null, a regra não se aplica.')
+          table.time('hora_finalizacao').nullable().comment('Caso seja um cobrador, ele terá acesso ao aplicativo apenas antes desse horário. Se definido como null, a regra não se aplica.')
+          table.boolean('ativo').notNullable().defaultTo(true).comment('Se valor for TRUE o mesmo não aparece nas listagens, exceto nas rotas de busca geral.')
+          table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
+          table.string('created_by', 150).notNullable()
+          table.timestamp('updated_at', { useTz: true }).nullable()
+          table.string('updated_by', 150).nullable()
+        })
+    }
+  }
+
+  /**
+   * Método 'down' da migração.
+   * Exclui a tabela 'usuarios' se ela existir.
+   *
+   * @public
+   * @returns {Promise<void>}
+   */
+  public async down(): Promise<void> {
+    this.schema.withSchema(this.schemaName)
+      .dropTableIfExists(this.tableName)
+  }
+}
---
+++ database/migrations/1694468337186_permissoes.ts
@@ -0,0 +1,69 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+/**
+ * Classe de migração para criar a tabela 'permissoes'.
+ *
+ * Esta migração verifica se a tabela 'permissoes' já existe no banco de dados.
+ * Se não existir, a tabela é criada com as colunas especificadas.
+ * Se já existir, nada é feito no método 'up'.
+ *
+ * @class
+ * @extends BaseSchema
+ */
+export default class extends BaseSchema {
+  /**
+   * Nome do esquema no banco de dados.
+   *
+   * @protected
+   * @type {string}
+   */
+  protected schemaName: string = 'public'
+
+  /**
+   * Nome da tabela que esta migração cria.
+   *
+   * @protected
+   * @type {string}
+   */
+  protected tableName: string = 'permissoes'
+
+  /**
+   * Método 'up' da migração.
+   * Cria a tabela 'permissoes' se ela não existir.
+   *
+   * @public
+   * @returns {Promise<void>}
+   */
+  public async up(): Promise<void> {
+    const hasTable = await this.schema
+      .withSchema(this.schemaName)
+      .hasTable(this.tableName)
+
+    if (!hasTable) {
+      this.schema.withSchema(this.schemaName)
+        .createTable(this.tableName, (table) => {
+          table.integer('usuario_id').notNullable().unsigned().references('id').inTable('public.usuarios').onDelete('CASCADE').onUpdate('CASCADE')
+          table.integer('modulo_id').notNullable().unsigned().references('id').inTable('public.modulos').onDelete('CASCADE').onUpdate('CASCADE')
+          table.integer('unidade_id').notNullable().unsigned().references('id').inTable('public.unidades').onDelete('CASCADE').onUpdate('CASCADE')
+          table.specificType('acao', 'character varying[]').notNullable()
+          table.boolean('ativo').notNullable().defaultTo(true).comment('Se valor for TRUE o mesmo não aparece nas listagens, exceto nas rotas de busca geral.')
+          table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
+          table.string('created_by', 150).notNullable()
+          table.timestamp('updated_at', { useTz: true }).nullable()
+          table.string('updated_by', 150).nullable()
+        })
+    }
+  }
+
+  /**
+   * Método 'down' da migração.
+   * Exclui a tabela 'permissoes' se ela existir.
+   *
+   * @public
+   * @returns {Promise<void>}
+   */
+  public async down(): Promise<void> {
+    this.schema.withSchema(this.schemaName)
+      .dropTableIfExists(this.tableName)
+  }
+}
