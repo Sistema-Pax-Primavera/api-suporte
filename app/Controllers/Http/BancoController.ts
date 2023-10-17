09fd11ef03de0f5e5c64f359b95ba82c717b219b
GiovaneMiranda15 <mirandagiovane7@gmail.com>
2023-10-17 11:16:51 -0400

    feat: Criação da controller BancoController, para manipulação do CRUD do model Banco

--- app/Controllers/Http/BancoController.ts
+++ app/Controllers/Http/BancoController.ts
@@ -1,33 +1,33 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import CustomErrorException from 'App/Exceptions/CustomErrorException'
-import SubCategoriaHistorico from 'App/Models/SubCategoriaHistorico'
-import CreateSubCategoriaHistoricoValidator from 'App/Validators/CreateSubCategoriaHistoricoValidator'
+import Banco from 'App/Models/Banco'
+import CreateBancoValidator from 'App/Validators/CreateBancoValidator'
 
-export default class SubCategoriaHistoricoController {
+export default class BancoController {
 
     /**
-     * Método para cadastrar sub categoria histórico.
+     * Método para cadastrar banco.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof SubCategoriaHistoricoController
+     * @memberof BancoController
      */
     public async cadastrar({ request, response, auth }: HttpContextContract): Promise<any> {
         try {
 
             // Valida os campos informados.
-            const { categoriaHistoricoId, descricao } = await request.validate(CreateSubCategoriaHistoricoValidator)
+            const { descricao } = await request.validate(CreateBancoValidator)
 
             // Insere o registro no banco de dados.
-            const subCategoriaHistorico = await SubCategoriaHistorico.create({
-                categoriaHistoricoId, descricao, 
+            const banco = await Banco.create({
+                descricao,
                 createdBy: auth.user?.nome
             })
 
             return response.status(201).send({
                 status: true,
                 message: 'Registro cadastrado com sucesso!',
-                data: subCategoriaHistorico
+                data: banco
             })
         } catch (error) {
             return response.status(500).send({
@@ -38,33 +38,32 @@ export default class SubCategoriaHistoricoController {
     }
 
     /**
-     * Método para atualizar sub categoria histórico.
+     * Método para atualizar banco.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof SubCategoriaHistoricoController
+     * @memberof BancoController
      */
     public async atualizar({ request, response, params, auth }: HttpContextContract): Promise<any> {
         try {
 
-            // Busca a sub categoria histórico pelo id informado.
-            const subCategoriaHistorico = await SubCategoriaHistorico.findOrFail(params.id)
+            // Busca o banco pelo id informado.
+            const banco = await Banco.findOrFail(params.id)
 
             // Valida os campos informados.
-            const { categoriaHistoricoId, descricao } = await request.validate(CreateSubCategoriaHistoricoValidator)
+            const { descricao } = await request.validate(CreateBancoValidator)
 
             // Atualiza o objeto com os dados novos.
-            subCategoriaHistorico.categoriaHistoricoId = categoriaHistoricoId
-            subCategoriaHistorico.descricao = descricao
-            subCategoriaHistorico.updatedBy = auth.user?.nome ?? null
+            banco.descricao = descricao
+            banco.updatedBy = auth.user?.nome ?? null
 
             // Persiste no banco o objeto atualizado.
-            await subCategoriaHistorico.save()
+            await banco.save()
 
             return response.status(200).send({
                 status: true,
                 message: 'Registro atualizado com sucesso',
-                data: subCategoriaHistorico
+                data: banco
             })
         } catch (error) {
             return response.status(500).send({
@@ -75,28 +74,28 @@ export default class SubCategoriaHistoricoController {
     }
 
     /**
-     * Método para ativar/inativar sub categoria histórico.
+     * Método para ativar/inativar banco.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof SubCategoriaHistoricoController
+     * @memberof BancoController
      */
     public async ativar({ response, params, auth }: HttpContextContract): Promise<any> {
         try {
-            // Busca a sub categoria histórico pelo id informado.
-            const subCategoriaHistorico = await SubCategoriaHistorico.findOrFail(params.id)
+            // Busca o banco pelo id informado.
+            const banco = await Banco.findOrFail(params.id)
 
             // Atualiza o objeto com os dados novos.
-            subCategoriaHistorico.ativo = !subCategoriaHistorico.ativo
-            subCategoriaHistorico.updatedBy = auth.user?.nome ?? null
+            banco.ativo = !banco.ativo
+            banco.updatedBy = auth.user?.nome ?? null
 
             // Persiste no banco o objeto atualizado.
-            await subCategoriaHistorico.save()
+            await banco.save()
 
             return response.status(200).send({
                 status: true,
-                message: `Registro ${subCategoriaHistorico.ativo ? 'ativado' : 'inativado'} com sucesso`,
-                data: subCategoriaHistorico
+                message: `Registro ${banco.ativo ? 'ativado' : 'inativado'} com sucesso`,
+                data: banco
             })
 
         } catch (error) {
@@ -108,26 +107,26 @@ export default class SubCategoriaHistoricoController {
     }
 
     /**
-     * Método para buscar todas as sub categorias.
+     * Método para buscar todos os bancos.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof SubCategoriaHistoricoController
+     * @memberof BancoController
      */
     public async buscarTodos({ response }: HttpContextContract): Promise<any> {
         try {
-            // Busca todas as sub categorias existentes.
-            const subCategorias = await SubCategoriaHistorico.query()
+            // Busca todos os bancos existentes.
+            const bancos = await Banco.query()
 
             // Verifica se não foi retornado nenhum registro.
-            if (subCategorias.length <= 0) {
+            if (bancos.length <= 0) {
                 throw new CustomErrorException("Nenhum registro encontrado", 404);
             }
 
             return response.status(200).send({
                 status: true,
                 message: `Registros retornados com sucesso`,
-                data: subCategorias
+                data: bancos
             })
 
         } catch (error) {
@@ -139,26 +138,26 @@ export default class SubCategoriaHistoricoController {
     }
 
     /**
-     * Método para buscar as sub categorias ativas.
+     * Método para buscar os bancos ativos.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof SubCategoriaHistoricoController
+     * @memberof BancoController
      */
     public async buscarAtivos({ response }: HttpContextContract): Promise<any> {
         try {
-            // Busca todas as sub categorias ativas.
-            const subCategorias = await SubCategoriaHistorico.query().where('ativo', true)
+            // Busca todos os bancos ativos.
+            const bancos = await Banco.query().where('ativo', true)
 
             // Verifica se não foi retornado nenhum registro.
-            if (subCategorias.length <= 0) {
+            if (bancos.length <= 0) {
                 throw new CustomErrorException("Nenhum registro encontrado", 404);
             }
 
             return response.status(200).send({
                 status: true,
                 message: `Registros retornados com sucesso`,
-                data: subCategorias
+                data: bancos
             })
 
         } catch (error) {
@@ -170,21 +169,21 @@ export default class SubCategoriaHistoricoController {
     }
 
     /**
-     * Método para buscar a sub categoria histórico por id.
+     * Método para buscar o banco por id.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof SubCategoriaHistoricoController
+     * @memberof BancoController
      */
     public async buscarPorId({ response, params }: HttpContextContract): Promise<any> {
         try {
-            // Busca a sub categoria histórico pelo id informado.
-            const subCategoriaHistorico = await SubCategoriaHistorico.findOrFail(params.id)
+            // Busca o banco pelo id informado.
+            const banco = await Banco.findOrFail(params.id)
 
             return response.status(200).send({
                 status: true,
                 message: `Registro retornado com sucesso`,
-                data: subCategoriaHistorico
+                data: banco
             })
 
         } catch (error) {
