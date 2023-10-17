beb7445b667915e0f16802b4d4d825e4c632265e
GiovaneMiranda15 <mirandagiovane7@gmail.com>
2023-10-17 10:39:07 -0400

    feat: Criação da controller RacaController, para manipulação do CRUD do model Raca

--- app/Controllers/Http/RacaController.ts
+++ app/Controllers/Http/RacaController.ts
@@ -1,25 +1,25 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import CustomErrorException from 'App/Exceptions/CustomErrorException'
-import Especie from 'App/Models/Especie'
-import CreateEspecieValidator from 'App/Validators/CreateEspecieValidator'
+import Raca from 'App/Models/Raca'
+import CreateRacaValidator from 'App/Validators/CreateRacaValidator'
 
-export default class EspecieController {
+export default class RacaController {
 
     /**
-     * Método para cadastrar espécie.
+     * Método para cadastrar raça.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof EspecieController
+     * @memberof RacaController
      */
     public async cadastrar({ request, response, auth }: HttpContextContract): Promise<any> {
         try {
 
             // Valida os campos informados.
-            const { descricao } = await request.validate(CreateEspecieValidator)
+            const { descricao } = await request.validate(CreateRacaValidator)
 
             // Insere o registro no banco de dados.
-            const especie = await Especie.create({
+            const raca = await Raca.create({
                 descricao,
                 createdBy: auth.user?.nome
             })
@@ -27,7 +27,7 @@ export default class EspecieController {
             return response.status(201).send({
                 status: true,
                 message: 'Registro cadastrado com sucesso!',
-                data: especie
+                data: raca
             })
         } catch (error) {
             return response.status(500).send({
@@ -38,32 +38,32 @@ export default class EspecieController {
     }
 
     /**
-     * Método para atualizar espécie.
+     * Método para atualizar raça.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof EspecieController
+     * @memberof RacaController
      */
     public async atualizar({ request, response, params, auth }: HttpContextContract): Promise<any> {
         try {
 
-            // Busca o espécie pelo id informado.
-            const especie = await Especie.findOrFail(params.id)
+            // Busca o raça pelo id informado.
+            const raca = await Raca.findOrFail(params.id)
 
             // Valida os campos informados.
-            const { descricao } = await request.validate(CreateEspecieValidator)
+            const { descricao } = await request.validate(CreateRacaValidator)
 
             // Atualiza o objeto com os dados novos.
-            especie.descricao = descricao
-            especie.updatedBy = auth.user?.nome ?? null
+            raca.descricao = descricao
+            raca.updatedBy = auth.user?.nome ?? null
 
             // Persiste no banco o objeto atualizado.
-            await especie.save()
+            await raca.save()
 
             return response.status(200).send({
                 status: true,
                 message: 'Registro atualizado com sucesso',
-                data: especie
+                data: raca
             })
         } catch (error) {
             return response.status(500).send({
@@ -74,28 +74,28 @@ export default class EspecieController {
     }
 
     /**
-     * Método para ativar/inativar espécie.
+     * Método para ativar/inativar raça.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof EspecieController
+     * @memberof RacaController
      */
     public async ativar({ response, params, auth }: HttpContextContract): Promise<any> {
         try {
-            // Busca a espécie pelo id informado.
-            const especie = await Especie.findOrFail(params.id)
+            // Busca a raça pelo id informado.
+            const raca = await Raca.findOrFail(params.id)
 
             // Atualiza o objeto com os dados novos.
-            especie.ativo = !especie.ativo
-            especie.updatedBy = auth.user?.nome ?? null
+            raca.ativo = !raca.ativo
+            raca.updatedBy = auth.user?.nome ?? null
 
             // Persiste no banco o objeto atualizado.
-            await especie.save()
+            await raca.save()
 
             return response.status(200).send({
                 status: true,
-                message: `Registro ${especie.ativo ? 'ativado' : 'inativado'} com sucesso`,
-                data: especie
+                message: `Registro ${raca.ativo ? 'ativado' : 'inativado'} com sucesso`,
+                data: raca
             })
 
         } catch (error) {
@@ -107,26 +107,26 @@ export default class EspecieController {
     }
 
     /**
-     * Método para buscar todos as espécies.
+     * Método para buscar todos as raças.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof EspecieController
+     * @memberof RacaController
      */
     public async buscarTodos({ response }: HttpContextContract): Promise<any> {
         try {
-            // Busca todas as espécies existentes.
-            const especies = await Especie.query()
+            // Busca todas as raças existentes.
+            const racas = await Raca.query()
 
             // Verifica se não foi retornado nenhum registro.
-            if (especies.length <= 0) {
+            if (racas.length <= 0) {
                 throw new CustomErrorException("Nenhum registro encontrado", 404);
             }
 
             return response.status(200).send({
                 status: true,
                 message: `Registros retornados com sucesso`,
-                data: especies
+                data: racas
             })
 
         } catch (error) {
@@ -138,26 +138,26 @@ export default class EspecieController {
     }
 
     /**
-     * Método para buscar as espécies ativas.
+     * Método para buscar as raças ativas.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof EspecieController
+     * @memberof RacaController
      */
     public async buscarAtivos({ response }: HttpContextContract): Promise<any> {
         try {
-            // Busca todas as espécies ativas.
-            const especies = await Especie.query().where('ativo', true)
+            // Busca todas as raças ativas.
+            const racas = await Raca.query().where('ativo', true)
 
             // Verifica se não foi retornado nenhum registro.
-            if (especies.length <= 0) {
+            if (racas.length <= 0) {
                 throw new CustomErrorException("Nenhum registro encontrado", 404);
             }
 
             return response.status(200).send({
                 status: true,
                 message: `Registros retornados com sucesso`,
-                data: especies
+                data: racas
             })
 
         } catch (error) {
@@ -169,21 +169,21 @@ export default class EspecieController {
     }
 
     /**
-     * Método para buscar a espécie por id.
+     * Método para buscar a raça por id.
      *
      * @param {HttpContextContract} ctx - O contexto da solicitação HTTP.
      * @return {*} 
-     * @memberof EspecieController
+     * @memberof RacaController
      */
     public async buscarPorId({ response, params }: HttpContextContract): Promise<any> {
         try {
-            // Busca a espécie pelo id informado.
-            const especie = await Especie.findOrFail(params.id)
+            // Busca a raça pelo id informado.
+            const raca = await Raca.findOrFail(params.id)
 
             return response.status(200).send({
                 status: true,
                 message: `Registro retornado com sucesso`,
-                data: especie
+                data: raca
             })
 
         } catch (error) {
