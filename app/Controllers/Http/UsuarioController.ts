a606b98283c75300b0077069ee3a311be10cc984
GiovaneMiranda15 <mirandagiovane7@gmail.com>
2023-11-14 15:55:08 -0400

    docs: Adicionado comentários as funções de vínculo

--- app/Controllers/Http/UsuarioController.ts
+++ app/Controllers/Http/UsuarioController.ts
@@ -12,7 +12,17 @@ import UpdateUsuarioValidator from 'App/Validators/UpdateUsuarioValidator'
 
 export default class UsuarioController {
 
+    /**
+     * Função para vincular as permissões do usuário.
+     *
+     * @private
+     * @param {any[]} permissoes
+     * @param {number} usuarioId
+     * @param {(string | null | undefined)} usuario
+     * @memberof UsuarioController
+     */
     private async vincularPermissoes(permissoes: any[], usuarioId: number, usuario: string | null | undefined) {
+        // Inativa todas as permissões existentes do usuário.
         await Permissao.query()
             .where('usuarioId', usuarioId)
             .update({ ativo: false, updatedBy: usuario })
@@ -26,7 +36,7 @@ export default class UsuarioController {
                 .where('ativo', true)
                 .first()
 
-            // Verifica se o módulo existe e está ativo.
+            // Verifica se a unidade existe e está ativa.
             let unidade = await Unidade.query()
                 .where('id', item.unidadeId)
                 .where('ativo', true)
@@ -45,18 +55,30 @@ export default class UsuarioController {
             }
         }
 
+        // Persiste no banco as permissões atualizadas.
         await Permissao.updateOrCreateMany(['moduloId', 'unidadeId', 'usuarioId'], permissoesFormatadas)
     }
 
+    /**
+     * Função para vincular o registro de cobrador ao usuário caso o mesmo possua essa função.
+     *
+     * @private
+     * @param {*} cobrador
+     * @memberof UsuarioController
+     */
     private async vincularCobrador(cobrador: any) {
-        const cob = await Cobrador.query().where('usuarioId', cobrador.usuarioId).first()
+        // Verifica se o usuário possui um registro de cobrador.
+        const cobradorAntigo = await Cobrador.query().where('usuarioId', cobrador.usuarioId).first()
 
-        if (cob) {
-            cob.merge({
+        // Caso seja encontrado algum registro, é feita a atualização, senão, é inserido um novo registro.
+        if (cobradorAntigo) {
+            cobradorAntigo.merge({
                 descricao: cobrador.descricao,
                 ativo: cobrador.ativo,
                 updatedBy: cobrador.usuario
             })
+
+            await cobradorAntigo.save()
         } else {
             await Cobrador.create({
                 usuarioId: cobrador.usuarioId,
@@ -137,8 +159,10 @@ export default class UsuarioController {
                 createdBy: auth.user?.nome
             })
 
+            // Chama a função para vincular as permissões.
             await this.vincularPermissoes(permissoes, usuario.id, auth.user?.nome)
 
+            // Verifica se o usuário possui a função de cobrador.
             const funcao = await Funcao.query()
                 .where('id', funcaoId)
                 .andWhereILike('descricao', '%COBRADOR%')
@@ -152,6 +176,7 @@ export default class UsuarioController {
                     ativo: true
                 }
 
+                // Chama a função de vínculo do cobrador.
                 await this.vincularCobrador(cobrador)
             }
 
@@ -198,8 +223,10 @@ export default class UsuarioController {
             // Persiste no banco o objeto atualizado.
             await usuario.save()
 
+            // Chama a função para vincular as permissões.
             await this.vincularPermissoes(permissoes, usuario.id, auth.user?.nome)
 
+            // Verifica se o usuário possui a função de cobrador.
             const funcao = await Funcao.query()
                 .where('id', funcaoId)
                 .andWhereILike('descricao', '%COBRADOR%')
@@ -213,6 +240,7 @@ export default class UsuarioController {
                     ativo: usuario.ativo
                 }
 
+                // Chama a função de vínculo do cobrador.
                 await this.vincularCobrador(cobrador)
             }
 
@@ -250,10 +278,12 @@ export default class UsuarioController {
             // Persiste no banco o objeto atualizado.
             await usuario.save()
 
+            // Ativa/Inativa as permissões do usuário.
             await Permissao.query()
                 .where('usuarioId', usuario.id)
                 .update({ ativo: usuario.ativo, updatedBy: auth.user?.nome })
 
+            // Ativa/Inativa o registro de cobrador.
             await Cobrador.query()
                 .where('usuarioId', usuario.id)
                 .update({ ativo: usuario.ativo, updatedBy: auth.user?.nome })
